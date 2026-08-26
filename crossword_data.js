c585b52753c59970467af7a3a841b0e40e99bbe2
Rhonotation <aledorh77@gmail.com>
2026-08-26 12:24:10 +0000

    Temporarily undisabled December 2024 and added size attributes.

--- crossword_data.js
+++ crossword_data.js
@@ -7,103 +7,119 @@ var crosswordData = [
     name: "October 2024",
     answer: ["3","4","7","6"],
     problemPdf: "2024_10P.pdf",
-    solutionPdf: "2024_10S.pdf"
+    solutionPdf: "2024_10S.pdf",
+    size: [2, 2]
   },
   {
     name: "November 2024",
     answer: ["3", "1", "2", "9"],
     problemPdf: "2024_11P.pdf",
-    solutionPdf: "2024_11S.pdf"
+    solutionPdf: "2024_11S.pdf",
+    size: [2, 2]
   },
   {
     name: "December 2024",
     answer: ["", "", "", ""],
     problemPdf: "2024_12P.pdf",
     solutionPdf: "2024_12S.pdf",
-    disabled: true
+    size: [3, 3]
   },
   {
     name: "January 2025",
     answer: ["1", "5", "2", "7"],
     problemPdf: "2025_01P.pdf",
-    solutionPdf: "2025_01S.pdf"
+    solutionPdf: "2025_01S.pdf",
+    size: [2, 2]
   },
   {
     name: "March 2025",
     answer: ["1", "4", "2", "3"],
     problemPdf: "2025_03P.pdf",
-    solutionPdf: "2025_03S.pdf"
+    solutionPdf: "2025_03S.pdf",
+    size: [2, 2]
   },
   {
     name: "April 2025",
     answer: ["1", "5", "1", "0"],
     problemPdf: "2025_04P.pdf",
-    solutionPdf: "2025_04S.pdf"
+    solutionPdf: "2025_04S.pdf",
+    size: [2, 2]
   },
   {
     name: "September 2025",
     answer: ["1", "2", "1", "9"],
     problemPdf: "2025_09P.pdf",
-    solutionPdf: "2025_09S.pdf"
+    solutionPdf: "2025_09S.pdf",
+    size: [2, 2]
   },
   {
     name: "October 2025",
     answer: ["2", "5", "1", "2"],
     problemPdf: "2025_10P.pdf",
-    solutionPdf: "2025_10S.pdf"
+    solutionPdf: "2025_10S.pdf",
+    size: [2, 2]
   },
   {
     name: "November 2025",
     answer: ["1", "1", "4", "8"],
     problemPdf: "2025_11P.pdf",
-    solutionPdf: "2025_11S.pdf"
+    solutionPdf: "2025_11S.pdf",
+    size: [2, 2]
   },
   {
     name: "December 2025",
     answer: ["1", "3", "6", "8"],
     problemPdf: "2025_12P.pdf",
-    solutionPdf: "2025_12S.pdf"
+    solutionPdf: "2025_12S.pdf",
+    size: [2, 2]
   },
   {
     name: "January 2026",
     answer: ["1", "3", "5", "9"],
     problemPdf: "2026_01P.pdf",
-    solutionPdf: "2026_01S.pdf"
+    solutionPdf: "2026_01S.pdf",
+    size: [2, 2]
   },
   {
     name: "February 2026",
     answer: ["1", "8", "3", "7"],
     problemPdf: "2026_02P.pdf",
-    solutionPdf: "2026_02S.pdf"
+    solutionPdf: "2026_02S.pdf",
+    size: [2, 2]
   },
   {
     name: "March 2026",
     answer: ["3", "5", "6", "6"],
     problemPdf: "2026_03P.pdf",
-    solutionPdf: "2026_03S.pdf"
+    solutionPdf: "2026_03S.pdf",
+    size: [2, 2]
   },
   {
     name: "April 2026",
     answer: ["1", "7", "2", "8"],
     problemPdf: "2026_04P.pdf",
-    solutionPdf: "2026_04S.pdf"
+    solutionPdf: "2026_04S.pdf",
+    size: [2, 2]
   },
   {
     name: "May 2026",
     answer: ["8", "1", "4", "9"],
     problemPdf: "2026_05P.pdf",
     solutionPdf: "2026_05S.pdf",
+    size: [2, 2]
   },
   {
     name: "June-July 2026",
     answer: ["6", "4", "3", "2"],
     problemPdf: "2026_06-07P.pdf",
     solutionPdf: "2026_06-07S.pdf",
+    size: [2, 2]
   },
   {
     name: "August 2026",
     answer: ["6", "3", "5", "3"],
     problemPdf: "2026_08P.pdf",
     solutionPdf: "2026_08S.pdf",
+    size: [2, 2]
   },
 ];
